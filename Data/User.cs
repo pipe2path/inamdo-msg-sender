729f44a030712807901eea391d6affec1b8ef376
unknown <kevinmenezes@yahoo.com>
2018-09-09 23:03:09 -0700

    get data, sinch sms

--- Data/User.cs
+++ Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,15 +9,21 @@ namespace InamdoMsgSender
 {
     public class User
     {
+
+        // not using userId because NewtonSoft cannot deserialize an ObjectId type. Throws an error. Will use code and userPhone for uniqueness.
+
+        [JsonConverter(typeof(ObjectIdConverter))]
+        public ObjectId userId { get; set; }
+
         //[BsonId]
-        //public ObjectId internalId { get; set; }
-                
-        public int surveyId { get; set; }
+        //[BsonRepresentation(BsonType.ObjectId)]
+        //public string internalId { get; set; }
+
+        public int code { get; set; }
         public string userName { get; set; }
         public string userPhone { get; set; }
         public string userEmail { get; set; }
         public string message { get; set; }
-        public int code { get; set; }
         public bool optIn { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using MongoDB.Bson;
+using Newtonsoft.Json;
+using Sinch.ServerSdk;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -17,10 +19,10 @@ namespace InamdoMsgSender
             var userTask = getUsers();
 
             userTask.ContinueWith(task => {
-                var users = task.Result;
-                foreach (User u in users)
-                    Console.WriteLine(u.userName.ToString());
-                
+                //var users = task.Result;
+                //foreach (User u in users)
+                //Console.WriteLine(u.userName.ToString());
+                Console.WriteLine("Processing done ...");
             },
             TaskContinuationOptions.OnlyOnRanToCompletion);
             Console.ReadLine();
@@ -29,15 +31,42 @@ namespace InamdoMsgSender
         static async Task<IEnumerable<User>> getUsers() 
         {
             string path = "http://localhost:56362/api/users/couponlist";
+            ObjectId userId = new ObjectId();
+            string phoneNum = "";
+            string smsMessage = "";
+            string code = "";
+            
             //await Task.Delay(3000);
 
             var response = await client.GetAsync(path);
             response.EnsureSuccessStatusCode();
             var stringResult = await response.Content.ReadAsStringAsync();
             IEnumerable<User> users = JsonConvert.DeserializeObject<IEnumerable<User>>(stringResult);
+            
+            // process messages
+            foreach (User u in users)
+            {
+                userId = u.userId;
+                phoneNum = u.userPhone;
+                code = u.code.ToString();
+                smsMessage = u.message + " Please use code: " + code + " when you order.";
+                var smsApi = SinchFactory.CreateApiFactory("86be6998-e82f-49eb-9d8d-cdd2427ad4a9", "5MnvbXXhe0iMuzXjl02WWQ==").CreateSmsApi();
+                var sendSmsResponse = await smsApi.Sms("+19094524127", smsMessage).Send();
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                var smsMessageStatusResponse = await smsApi.GetSmsStatus(sendSmsResponse.MessageId);
+
+                if (smsMessageStatusResponse.Status == "successful")
+                {
+                    // update db
+                }
+
+
+            }
+
+
+
+
             return users;
         }
-        
-        
     }
 }
